99d9da83c8b2a633606fb6c17dc080b823f012b4
itzsimpleboii8282 <72684942+itzsimpleboii8282@users.noreply.github.com>
2021-01-30 17:11:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+import requests
+name = input("Enter a song name for getting lyrics : ")
+lyrics = requests.get(f'https://some-random-api.ml/lyrics?title={name}')
+lyricsdata = lyrics.json()
+songname = (str(lyricsdata['title']))
+songauthor = (str(lyricsdata['author']))
+songlyrics = (str(lyricsdata['lyrics']))
+print('\n\n')
+print(f'Song name is ---- {songname}.\nThe author is ---- {songauthor}\n\n And the lyrics are :-\n\n {songlyrics}\n')
